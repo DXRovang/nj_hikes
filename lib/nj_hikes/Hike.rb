e7820af075a38280ceb15a64b092b54fac7f3bff
Alexander Rovang <wolf_vii@yahoo.com>
2021-01-06 17:47:37 -0500

    YES

--- lib/nj_hikes/Hike.rb
+++ lib/nj_hikes/Hike.rb
@@ -20,9 +20,11 @@ class NjHikes::Hike
     location = self.coordinates.split
     one = location[0].scan(/\d{2}[.]\d{5}/)
     two = location[1].scan(/\d{2}[.]\d{5}/)#needs to become negative
-    final_location = [one, two].join(" ").split
+    two_minus = (two.join.to_f * -1).to_s.split
+    final_location = [one, two_minus].join(" ").split
     miles = Geocoder::Calculations.distance_between(NjHikes::CLI.hiker.location, final_location)
     miles.round
   end 
 
 end
+
