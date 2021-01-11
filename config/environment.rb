4121bddf9cd064a1ef20c6132ee24f2466e28253
Alexander Rovang <wolf_vii@yahoo.com>
2021-01-11 12:22:52 -0500

    added config

---
+++ config/environment.rb
@@ -0,0 +1,12 @@
+
+require "nokogiri"
+require "open-uri"
+require "pry"
+require "geocoder"
+require "colorize"
+
+require_relative "../lib/nj_hikes" #curious as to the neccessity for this
+require_relative "../lib/nj_hikes/CLI"
+require_relative "../lib/nj_hikes/Hike"
+require_relative "../lib/nj_hikes/Scraper"
+require_relative "../lib/nj_hikes/User"
--- lib/nj_hikes.rb
+++ lib/nj_hikes.rb
@@ -1,14 +1,15 @@
 require_relative "./nj_hikes/version"
-require_relative "./nj_hikes/CLI"
-require_relative "./nj_hikes/User"
-require_relative "./nj_hikes/Hike"
-require_relative "./nj_hikes/Scraper"
 
-require "nokogiri"
-require "open-uri"
-require "pry"
-require "geocoder"
-require "colorize"
+# require_relative "./nj_hikes/CLI"
+# require_relative "./nj_hikes/User"
+# require_relative "./nj_hikes/Hike"
+# require_relative "./nj_hikes/Scraper"
+
+# require "nokogiri"
+# require "open-uri"
+# require "pry"
+# require "geocoder"
+# require "colorize"
 
 
 module NjHikes
